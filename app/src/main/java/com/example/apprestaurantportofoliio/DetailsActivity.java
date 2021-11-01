75ae58db0874bdb934821637a9406e61fc8054c2
YuniWulan <keepcalm.upnormal@gmail.com>
2021-11-01 22:02:19 +0700

    first commit

---
+++ app/src/main/java/com/example/apprestaurantportofoliio/DetailsActivity.java
@@ -0,0 +1,23 @@
+
+package com.example.apprestaurantportofoliio;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.squareup.picasso.Picasso;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+    }
+}
